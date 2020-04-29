e020be0bc9152c90b0fe8992d402d51db65abcfd
lsl-git-hub <64401211+lsl-git-hub@users.noreply.github.com>
2020-04-29 12:09:40 +0800

    Add files via upload

---
+++ name_cases3.py
@@ -0,0 +1,27 @@
+# 2-3个性化消息
+name = 'lsl'
+print('Hello '+name.title()+',would you like to learn more')
+# 2-4调整名字的大小写
+print(name.upper())
+print(name.lower())
+print(name.title())
+# 2-5名言 2-6名言2
+famous_name = 'Albert Einstein'
+famous_say = '"A person who never made a mistake never tried any thing new."'
+message = famous_name+' once said, '+famous_say
+print(message)
+# 2-7剔除人名中的空白
+name_case1 = "\n  \tshuli  "
+print(name_case1)
+print(name_case1.rstrip())
+print(name_case1.lstrip())
+print(name_case1.strip())
+# 2-8数字8
+print(2+6)
+print(10-2)
+print(2*4)
+print(2**3)
+print(16/2)
+# 2-9最喜欢的数字
+number = 9
+print('my favorite number is '+str(number))
